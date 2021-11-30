0d5727a03b44b05e0486fc903ad50c2b13ef250e
sunbeanp <sunbeanp@gmail.com>
2021-11-30 05:48:49 -0800

    Encrypt Scoreboard using RSA

--- modules/sprites/__init__.py
+++ modules/sprites/__init__.py
@@ -1,4 +1,5 @@
 '''초기화'''
 from .dinosaur import Dinosaur
-from .obstacle import Cactus, Ptera, Apple
+from .obstacle import Cactus, Ptera
 from .scene import Ground, Cloud, Scoreboard
+from .rsa import encrypt, decrypt
